412e8b3e981f675475e9455a3ccdb661a0c4df57
alfadjri28 <alfadjri28@gmail.com>
2026-05-23 14:58:30 +0700

    comper

---
+++ Park_2_tipe_data/park_5_comper.py
@@ -0,0 +1,24 @@
+x = 2
+y = 2
+
+# Besar dari (>)
+hasil = x > y
+print(f"Apakah nilai dari {x} Besar dari (>) {y} adalah {hasil}")
+# Kecil dari (<)
+hasil = x < y
+print(f"Apakah nilai dari {x} Kecil dari (<) {y} adalah {hasil}")
+# Besar dari sama dengan (>=)
+hasil = x >= y
+print(f"Apakah nilai dari {x} Besar dari samadengan (>=) {y} adalah {hasil}")
+# Kecil dari sama dengan (<=)
+hasil = x <= y
+print(f"Apakah nilai dari {x} Kecil dari sama dengan (<=) {y} adalah {hasil}")
+h = "1"
+i = 1
+# sama dengan (==)
+hasil = i == h
+print(f"Apakah nilai dari {h} sama dengan (==) {i} adalah {hasil}")
+
+kondisi = True
+hasil = kondisi != True
+print(f"Apakah {kondisi} tidak sama dengan True adalah {hasil}")
