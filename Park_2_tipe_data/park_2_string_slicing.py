e8156ef68ef9cb54151a561ee75de38127a4e92e
alfadjri28 <alfadjri28@gmail.com>
2026-05-23 14:13:08 +0700

    string slicing

---
+++ Park_2_tipe_data/park_2_string_slicing.py
@@ -0,0 +1,11 @@
+nama_lengkap = "Alfadjri Dwi Fadhilah"
+
+print("Nama Lengkap : {0}".format(nama_lengkap))
+print("Cetak huruf ke 2 : {0}".format(nama_lengkap[1]))
+print("Kata pertama : {0}".format(nama_lengkap[:8]))
+print("Kata di tengah : {0}".format(nama_lengkap[9:12]))
+print("Kata terakhir : {0}".format(nama_lengkap[13:]))
+nominal = 20000
+print("Cetak kode awal Rp.nominal : {0}".format("Rp." + str(nominal)))
+kata = "kata"
+print("{0}".format(kata*10))
